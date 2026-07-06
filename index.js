af0f71a3e7cfeed52e13c0fa556e889a71048d1e
Yuta Aizawa <zawa929292@gmail.com>
2026-07-07 00:42:13 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function greet(){
   console.log("Hello from the main branch!");
 }
+=======
+function greet(){
+    console.log("Hello from thhe local branch");
+}
+>>>>>>> 990dfe2 ( 私たちの修正)
